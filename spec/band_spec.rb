9ca3cf3e88707e0c82d84773e242e5b2f4b2b95f
Jim <wrench1138@gmail.com>
2015-05-15 10:21:18 -0700

    add spec to ensure many to many relationship

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,6 +2,15 @@ require('spec_helper')
 require('rspec')
 require('band')
 
-describe (Band) do
-  it { should have_many(:venues) }
+# describe (Band) do
+#   it { should have_many(:venues) }
+# end
+
+describe(Band) do
+  it("has many venues") do
+    band = Band.create({:name => "band"})
+    venue1 = Venue.create({:name => "venue1"})
+    venue2 = Venue.create({:name => "venue2"})
+    expect(band.venues()).to(eq([venue1, venue2]))
+  end
 end
